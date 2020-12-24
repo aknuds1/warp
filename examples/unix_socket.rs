207bbbf457eb16e4e9fe5ce4a2031636eafa7c99
Paolo Barbolini <paolo@paolo565.org>
2020-12-24 20:56:43 +0100

    fix remaining compile errors

--- examples/unix_socket.rs
+++ examples/unix_socket.rs
@@ -1,5 +1,6 @@
 #![deny(warnings)]
 
+use async_stream::stream;
 use tokio::net::UnixListener;
 
 #[tokio::main]
@@ -7,7 +8,11 @@ async fn main() {
     pretty_env_logger::init();
 
     let listener = UnixListener::bind("/tmp/warp.sock").unwrap();
-    let incoming = listener.incoming();
+    let incoming = stream! {
+        while let item = listener.accept().await {
+            yield item.map(|item| item.0);
+        }
+    };
     warp::serve(warp::fs::dir("examples/dir"))
         .run_incoming(incoming)
         .await;
